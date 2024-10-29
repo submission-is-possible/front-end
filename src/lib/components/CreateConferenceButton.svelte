2eff70bc3283d3917c834798ef6a3146045a5ec4
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-10-29 13:28:21 +0100

    CreateConference button navigation

--- src/lib/components/CreateConferenceButton.svelte
+++ src/lib/components/CreateConferenceButton.svelte
@@ -1,17 +1,21 @@
-<script>
+<script lang="ts">
   import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
 
   const conferencePath = '/conference';
+  const conferenceCreationPath = '/conference/create-new';
 
-  // For debugging, let's add multiple logging approaches
-  console.log('Initial pathname:', $page?.url?.pathname); // Safe access
-  console.log('Whole page store:', $page); // Log entire store
+  // Go to conference-creation
+  function gotoConferenceCreation() {
+    goto(conferenceCreationPath);
+  }
 
 </script>
 
 {#if $page?.url?.pathname === conferencePath}
   <button 
-    aria-label="CreateConferenceButton" 
+    aria-label="CreateConferenceButton"
+    on:click={gotoConferenceCreation}
     class="flex items-center justify-center bg-primary hover:bg-blue-600 text-white font-medium rounded-full w-20 h-20 shadow-lg transition-colors duration-200">
     <div class="flex items-center justify-center">
       <iconify-icon icon="material-symbols:add-rounded" width="40" height="40"></iconify-icon>
