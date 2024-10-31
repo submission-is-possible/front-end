6400b3f761aeb15fabb9c86b56a082d1799eee83
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-10-31 09:52:09 +0100

    SIP-30 Fixed Footer end of the page positioning and create new confernce button placed in the correct position

--- src/lib/components/CreateConferenceButton.svelte
+++ src/lib/components/CreateConferenceButton.svelte
@@ -16,7 +16,7 @@
   <button 
     aria-label="CreateConferenceButton"
     on:click={gotoConferenceCreation}
-    class="flex items-center justify-center bg-primary hover:bg-blue-600 text-white font-medium rounded-full w-20 h-20 shadow-lg transition-colors duration-200">
+    class="flex items-center justify-center btn btn-primary text-white font-medium rounded-full w-20 h-20 shadow-lg transition-colors duration-200">
     <div class="flex items-center justify-center">
       <iconify-icon icon="material-symbols:add-rounded" width="40" height="40"></iconify-icon>
     </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
   import '../app.css';
-  import Footer from '../lib/components/Footer.svelte';
-  import Navbar from '../lib/components/Navbar.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+  import Navbar from '$lib/components/Navbar.svelte';
   import CreateConferenceButton from '$lib/components/CreateConferenceButton.svelte';
 </script>
 
-<div class="min-h-screen flex flex-col">
-  <Navbar />
-
-  <main class="flex justify-center items-center h-screen">
+<div class="min-h-screen flex flex-col justify-between">
+  <div class="flex flex-col flex-grow"> <!-- Added wrapper div -->
+    <Navbar />
+    <main class="flex flex-grow">
       <div class="w-1/3 p-4"></div>
       <div class="w-2/4 p-4">
         <div class="container px-4 pb-20">
           <slot />
         </div>
       </div>
-      <div class="w-1/3 p-4"></div>
-    
-
-    <div class="fixed bottom-4 right-4 z-10 mb-64">
-      <CreateConferenceButton />
-    </div>
-
-  </main>
+      <div class="w-1/3 p-4 flex flex-col justify-between">
+        <div></div>
+        <div class="flex justify-end">
+          <CreateConferenceButton />
+        </div>
+      </div>
+    </main>
+  </div>
   <Footer />
 </div>
