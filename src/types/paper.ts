b40c1c4242a9759c8aeba81b412f73f6b78d042a
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2024-11-13 09:46:30 +0100

    SIP-85 Added paper type

---
+++ src/types/paper.ts
@@ -0,0 +1,10 @@
+type Paper = {
+    id: number;
+    title: string;
+    paper_file: string;
+    conference: number;
+    conference_title: string;
+    author: string;
+    status: string;
+    created_at: string;
+}
